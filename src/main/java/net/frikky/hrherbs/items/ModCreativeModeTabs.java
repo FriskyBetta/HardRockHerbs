858f1308645eab33e71850065f992ea32b0adb38
FriskyBetta <friskybetta@gmail.com>
2024-09-17 17:37:12 -0400

    Removed unused imports

--- src/main/java/net/frikky/hrherbs/items/ModCreativeModeTabs.java
+++ src/main/java/net/frikky/hrherbs/items/ModCreativeModeTabs.java
@@ -4,11 +4,10 @@ import net.frikky.hrherbs.HardRockPlants;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegisterEvent;
+
 import net.minecraftforge.registries.RegistryObject;
 
 
