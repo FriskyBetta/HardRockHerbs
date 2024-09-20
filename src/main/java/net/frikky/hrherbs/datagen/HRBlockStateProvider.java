7e9aeb22df14609bef9f665fc9fe0f48d0678d4c
FriskyBetta <friskybetta@gmail.com>
2024-09-20 11:20:38 -0400

    BlockState Provider data gen class

--- src/main/java/net/frikky/hrherbs/datagen/HRBlockStateProvider.java
+++ src/main/java/net/frikky/hrherbs/datagen/HRBlockStateProvider.java
@@ -1,4 +1,26 @@
 package net.frikky.hrherbs.datagen;
 
-public class HRBlockStateProvider {
+import net.frikky.hrherbs.HardRockPlants;
+import net.frikky.hrherbs.block.HRBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class HRBlockStateProvider extends BlockStateProvider {
+
+    public HRBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, HardRockPlants.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(HRBlocks.PLACEHOLDER_BLOCK);
+
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
 }
