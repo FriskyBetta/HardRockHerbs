3878de4e61808254fc1f867640b647e295c055fe
FriskyBetta <friskybetta@gmail.com>
2024-09-20 12:13:32 -0400

    Loot Tables Data Gen

--- src/main/java/net/frikky/hrherbs/datagen/HRLootTableProvider.java
+++ src/main/java/net/frikky/hrherbs/datagen/HRLootTableProvider.java
@@ -1,4 +1,17 @@
 package net.frikky.hrherbs.datagen;
 
+import net.frikky.hrherbs.datagen.loot.HRBlockLootTables;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+
+import java.util.List;
+import java.util.Set;
+
 public class HRLootTableProvider {
+    public static LootTableProvider create(PackOutput output) {
+        return new LootTableProvider(output, Set.of(), List.of(
+                new LootTableProvider.SubProviderEntry(HRBlockLootTables::new, LootContextParamSets.BLOCK)
+        ));
+    }
 }
--- src/main/java/net/frikky/hrherbs/datagen/loot/HRBlockLootTables.java
+++ src/main/java/net/frikky/hrherbs/datagen/loot/HRBlockLootTables.java
@@ -1,4 +1,29 @@
 package net.frikky.hrherbs.datagen.loot;
 
-public class HRBlockLootTables {
+import net.frikky.hrherbs.block.HRBlocks;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.Set;
+
+public class HRBlockLootTables extends BlockLootSubProvider {
+
+    public HRBlockLootTables() {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+    }
+
+    @Override
+    protected void generate() {
+        this.dropSelf(HRBlocks.PLACEHOLDER_BLOCK.get());
+
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return HRBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+    }
 }
