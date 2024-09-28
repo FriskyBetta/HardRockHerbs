edb08487070598780646afa1db946b58f3a204f6
FriskyBetta <friskybetta@gmail.com>
2024-09-28 15:05:23 -0400

    Began JEI compat

---
+++ src/main/java/net/frikky/hrherbs/compat/JEIModPlugin.java
@@ -0,0 +1,33 @@
+package net.frikky.hrherbs.compat;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.frikky.hrherbs.HardRockPlants;
+import net.minecraft.resources.ResourceLocation;
+
+@JeiPlugin
+public class JEIModPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(HardRockPlants.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        IModPlugin.super.registerCategories(registration);
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        IModPlugin.super.registerRecipes(registration);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        IModPlugin.super.registerGuiHandlers(registration);
+    }
+
+}
